acae230847d1e706c128f8c6fdd7b4881a4d73a0
Riels <289770334@qq.com>
2019-06-15 16:35:45 +0800

    'layout'

--- tktest.py
+++ tktest.py
@@ -4,18 +4,17 @@ import tkinter.filedialog
 from PIL import Image, ImageTk
 
 
-def upload():
-
-    pass
-
-
+def inference(img):
+    den_map = img
+    return den_map, 0
 
 
-
+# todo: 获取绑定函数的返回值；布局
 class App:
     def __init__(self, master, options):
-
-        fm_title = Frame(master)
+        self.master = master
+        self.options = options
+        fm_title = Frame(self.master)
         Label(fm_title, text='(1)select model\n(2)upload an image',    # 标签的文字
               bg='green',                 # 背景颜色
               font=('Arial', 12),         # 字体和字体大小
@@ -24,32 +23,64 @@ class App:
 
         variable = StringVar()
         variable.set(options[0])
+        self.intVar = IntVar()
         # 使用Frame增加一层容器
-        fm_select = Frame(master)
+        fm_select = Frame(self.master)
         for i, option in enumerate(options):
-            Radiobutton(fm_select, text=option, value=i).pack(anchor=W)
+            Radiobutton(fm_select, text=option, value=i, variable=self.intVar,
+                        command=self.change,).grid(row=i, column=0, sticky=W)
+        # Button(fm_select, text='print', command=self.print_radio).grid(row=0, column=1, columnspan=2)
+        self.alg_label = Label(fm_select, text=self.options[int(self.intVar.get())])
+        self.alg_label.grid(row=0, column=1, columnspan=2)
         fm_select.place(x=50, y=100, anchor=NW)
 
-        fm_upload = Frame(master)
+        fm_upload = Frame(self.master)
+
+        Button(fm_upload, text="选择文件", command=lambda: self.choose_file()).pack(side=LEFT)
+        # Button(fm_upload, text="上传", command=self.upload).pack(side=LEFT)
+        fm_upload.place(x=300, y=100, anchor=NW)
+
         # 要展示的图片
         self.photo = None
-        Button(fm_upload, text="选择文件", command=lambda:self.choose_file(fm_upload)).pack(side=LEFT)
-        Button(fm_upload, text="上传", command=upload).pack(side=LEFT)
-        fm_upload.place(x=200, y=100, anchor=NW)
+        self.den_map = None
 
-    def choose_file(self, fm):
+    def change(self):
+        self.alg_label['text'] = self.options[int(self.intVar.get())]
+
+    def choose_file(self):
+        fm_img = Frame(self.master)
         filename = tkinter.filedialog.askopenfilename(filetypes=[('All Files', '*')],
                                                       initialdir='~/Pictures')
-        img = Image.open(filename)  # 打开图片
-        img = img.resize((400, 300))
+        # 什么都没选
+        if not filename:
+            return
+        raw_img = Image.open(filename)  # 打开图片
+
+        img = raw_img.resize((400, 300))
         self.photo = ImageTk.PhotoImage(img)
-        img_label = Label(fm, image=self.photo)
-        img_label.pack(side=TOP)
+        img_label = Label(fm_img, image=self.photo)
+        img_label.pack(side=LEFT, anchor=NW)
+
+        den_map, count = inference(raw_img)
+        den_map = den_map.resize((400, 300))
+        self.den_map = ImageTk.PhotoImage(den_map)
+        dmp_label = Label(fm_img, image=self.den_map)
+        dmp_label.pack(side=LEFT, anchor=NW)
+
+        Label(fm_img, text='估计人数: '+str(count)).pack(side=LEFT, anchor=NW)
+
+        fm_img.place(x=0, y=150, anchor=NW)
         print(filename)
+
+    def upload(self):
+        pass
+
+    def print_radio(self):
+        print(self.options[int(self.intVar.get())])
 
 
 root = Tk()
-root.geometry('800x600')
+root.geometry('1000x800')
 root.title("crowd count demo")
 model_list = ["csrnet", "new dilated"]
 
